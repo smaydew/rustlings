684b40d6936e521a5051514edfdb00263bc7a53f
Stacy Maydew <stacy.maydew@starlab.io>
2022-04-22 11:26:16 -0600

    Completed rustlings exercises included.

---
+++ rustlings/exercises/clippy/target/debug/deps/clippy2-a0862d0389b89742.d
@@ -0,0 +1,7 @@
+/home/smaydew/projects/rustlings/exercises/clippy/target/debug/deps/clippy2-a0862d0389b89742.rmeta: clippy2.rs
+
+/home/smaydew/projects/rustlings/exercises/clippy/target/debug/deps/clippy2-a0862d0389b89742.d: clippy2.rs
+
+clippy2.rs:
+
+# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__-D__CLIPPY_HACKERY__clippy::float_cmp__CLIPPY_HACKERY__
